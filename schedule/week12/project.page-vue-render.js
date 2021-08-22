3ceaeec829c500ee6d8a81ebf73e99d61e28d651
Akshay Narayan @ HP360 (MyOfficeThing) <okkhoy@gmail.com>
2021-08-22 23:21:12 +0800

    Site Update. [skip ci]

--- schedule/week12/project.page-vue-render.js
+++ schedule/week12/project.page-vue-render.js
@@ -86,6 +86,6 @@ with(this){return _c('ul',[_c('li',[_c('strong',[_v("Create the first version of
 with(this){return _c('span',[_c('span',[_c('span',{staticClass:"fas fa-users",attrs:{"aria-hidden":"true"}})]),_v(" Double-check RepoSense compatibility")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.6")]),_v(", generated on Mon, 16 Aug 2021, 0:00:26 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.6")]),_v(", generated on Sun, 22 Aug 2021, 23:07:20 GMT+8]")])])])}
 }];
   
